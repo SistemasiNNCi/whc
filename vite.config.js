442a7dd9efc98a549892b50cd637032b8a87fb7a
alejandro-casillas <alejandro.casillas.anguiano@gmail.com>
2025-08-27 15:36:32 -0600

    build: revertir configuración UMD y restaurar build de app para Netlify

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    lib: {
-      entry: "src/mountWHC.jsx",
-      name: "WHC",
-      fileName: "whc-widgets",
-      formats: ["umd"],
-    },
-    rollupOptions: {
-      external: [],
-    },
-  },
 });
